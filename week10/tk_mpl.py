2025027472f6e00351167c7aef540f665fbaede5
skooter500 <skooter500@gmail.com>
2025-11-24 10:14:51 +0000

    tk

--- week10/tk_mpl.py
+++ week10/tk_mpl.py
@@ -1,12 +1,31 @@
 import tkinter as tk
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
-
+import pandas as pd
+import sqlite3
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
+albums_df = pd.read_csv("data/album.csv")
+albums_by_artist = albums_df['artist'].value_counts()
+print(albums_by_artist.head())
+
+# artists = 
+
+'''
+conn = sqlite3.connect('data/tunes.db')
+cursor = conn.cursor()
+
+# Create table
+cursor.execute('select count(*) as count, artist from albums group by artist')
+
+albums_by_artist = cursor.fetchall()
+
+print(albums_df.head())
+'''
 def on_click():
-    label.config(text=f"Hello " + entry.get() )
+    ax1.set_title(entry.get())
+    canvas.draw()
 
 
 root = tk.Tk()
@@ -18,9 +37,9 @@ y1 = [3, 24, 17, 22]
 
 categories = ["Car", "Bus", "Bike", "Walk"]
 values = [10, 8, 8, 1]
+d
 
-
-ax1.plot(x, y1, color='red')
+ax1.plot(albums_by_artist.index.tolist(), albums_by_artist.values.tolist(), color='red')
 ax1.set_title('First Plot')
 ax2.bar(categories, values, color='blue')
 ax2.set_title('Second Plot')
